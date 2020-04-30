839794c745d683d671021568c02f2384de336b61
Abi Vickery <vickeryabi@gmail.com>
2020-04-30 10:49:06 -0500

    first two entries

--- mlpchar.sql
+++ mlpchar.sql
@@ -9,5 +9,10 @@ CREATE TABLE characters(
     gender VARCHAR(45),
     residence VARCHAR(75),
     occupation VARCHAR(100),
+    family VARCHAR(45),
     PRIMARY KEY(id)
 );
+
+INSERT INTO characters (name, race, gender, residence, occupation, family) values ('Ace Point', 'Earth Pony', 'Male', 'Equestria', 'Tennis Pony', 'unknown', 1);
+
+INSERT INTO characters (name, race, gender, residence, occupation, family) values ('Amira*', 'Earth Pony', 'Female', 'Saddle Arabia', 'Saddle Arabia Delegate', 'unknown', 2);
